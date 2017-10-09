057201305e6bbfdff3a8fcf4cefafc26de0c019b
kablondino <kablondino@gmail.com>
2017-10-09 21:20:18 +0200

    Naive approach to see if I understand

--- Model_Source/Naive_time_dep/Naive_time_dep.py
+++ Model_Source/Naive_time_dep/Naive_time_dep.py
@@ -7,6 +7,11 @@ mesh = IntervalMesh(nx, 0, 1)
 V = FunctionSpace(mesh, 'P', 1)
 x = SpatialCoordinate(mesh)
 
+# Time dependence
+Ttotal = 2.0
+num_steps = 50
+dt = Ttotal / num_steps
+
 # Boundary condition
 Z_D = Constant(0)
 
@@ -16,9 +21,12 @@ def boundary(x, on_boundary):
 bc = DirichletBC(V, Z_D, boundary)
 
 # Initial value
+#Z_0 = Expression('pow(x,2)')
 Z_0 = Constant(0)
+Z_n = interpolate(Z_0, V)
 
 # Define Constants
+epsilon = 1
 mu = 1
 A = 1
 B = 1
@@ -29,21 +37,32 @@ D = 1
 v = TestFunction(V)
 Z = Function(V)
 f = A + B*Z + C*Z**2 + D*Z**3
-F = (mu*dot(grad(Z), grad(v)))*dx - f*v*dx
+F = (epsilon*Z*v + dt*mu*dot(grad(Z), grad(v)))*dx - (Z_n + dt*f)*v*dx
 
-vtkfile = File('naive_time_ind.pvd')
+a, L = lhs(F), rhs(F)
 
-# Compute Solution
-solve(F == 0, Z, bc)
+vtkfile = File('data/naive_time_dep.pvd')
 
-vtkfile << Z
+t = 0
+for n in range(num_steps):
+	# Update time
+	t += dt
 
-plot(Z)
+	# Compute Solution
+	solve(F == 0, Z, bc, solver_parameters={"newton_solver":{"maximum_iterations":1000}})
 
-# Print out raw data to output
-Z_nodal_values = Z.vector()
-Z_array = Z_nodal_values.array()
+	# Write to vtu file
+	vtkfile << (Z, t)
 
-for i in range(len(Z_array)):
-	    print '%g %g' % (mesh.coordinates()[i], Z_array[i])
+	# Print out raw data to output
+	Z_nodal_values = Z.vector()
+	Z_array = Z_nodal_values.array()
+
+	for i in range(len(Z_array)):
+		print '%g %g %g' % (t, mesh.coordinates()[i], Z_array[i])
+
+	plot(Z)
+
+	# Reassign old value to new
+	Z_n.assign(Z)
 
