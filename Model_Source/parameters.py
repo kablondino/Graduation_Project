f2098742b529bdeb9b4ef2da72306c4e08e69f09
kablondino <kablondino@gmail.com>
2017-10-13 12:52:57 +0200

    Properly made lambdas

--- Model_Source/parameters.py
+++ Model_Source/parameters.py
@@ -13,10 +13,11 @@ mu_0 = 4*pi*1.0e-7
 R_cx = 1.0e-14
 
 # Plasma variables n_e, T_i, n_0
-n = 1.0e19		# CHANGE TO FUNCTION!?
+n = lambda x: (1 - x) * 1.0e19
+T = lambda x: (1 - x) * 2.0e3
 np = 1.0e19
 Tp = 1.0e3
-n_0 = lambda T: 4.0e17 * a_in0 * ( T / 100 )**(3.0/4)
+n_0 = lambda x: 4.0e17 * a_in0 * ( T(x) / 100 )**(3.0/4)
 
 # ASDEX-U specifications
 a_v = 0.8	# Vertical minor radius
@@ -38,25 +39,36 @@ L_nn = L_n / W_ped
 L_Tn = L_T / W_ped
 
 # Parameters
-e_p = 1 + (m_i*n + m_e*n) / (epsilon_0 * B**2)
-v_Ti = lambda T: (2 * e * T / m_i)**(1.0/2)
-v_Te = lambda T: (2 * e * T / m_e)**(1.0/2)
-rho_pi = lambda T: m_i * v_Ti(T) / (e * B_p)
-rho_pe = lambda T: m_e * v_Te(T) / (e * B_p)
-omega_bi = lambda T: aspect**(3.0/2) * v_Ti(T) / (q * R)
-omega_be = lambda T: aspect**(3.0/2) * v_Te(T) / (q * R)
+e_p = lambda X: 1.0 + (m_i*n(X) + m_e*n(X)) / (epsilon_0 * B**2)
+
+v_Ti = lambda X: (2 * e * T(X) / m_i)**(1.0/2)
+
+v_Te = lambda X: (2 * e * T(X) / m_e)**(1.0/2)
+
+rho_pi = lambda X: m_i * v_Ti(X) / (e * B_p)
+
+rho_pe = lambda X: m_e * v_Te(X) / (e * B_p)
+
+omega_bi = lambda X: aspect**(3.0/2) * v_Ti(X) / (q * R)
+
+omega_be = lambda X: aspect**(3.0/2) * v_Te(X) / (q * R)
 
 # Frequencies
-nu_ei = lambda T: 1.33e5 * (n * 1.0e-20) / (T * 1.0e-3)**(3.0/2)
-nu_ii = lambda T: 1.2 * (m_e / m_i)**(1.0/2) * nu_ei(T)
-nu_in0 = lambda T: a_in0 * omega_bi(T)
-nu_eff = lambda T: nu_ii(T) + nu_in0(T)
-nu_ai = lambda T: nu_ii(T) / omega_bi(T)	# nu_*i
-nu_ae = lambda T: nu_ei(T) / omega_be(T)	# nu_*e (not used as of now)
+nu_ei = lambda X: 1.33e5 * (n(X) * 1.0e-20) / (T(X) * 1.0e-3)**(3.0/2)
+
+nu_ii = lambda X: 1.2 * (m_e / m_i)**(1.0/2) * nu_ei(X)
+
+nu_in0 = lambda X: a_in0 * omega_bi(X)
+
+nu_eff = lambda X: nu_ii(X) + nu_in0(X)
+
+nu_ai = lambda X: nu_ii(X) / omega_bi(X)	# nu_*i
+
+nu_ae = lambda X: nu_ei(X) / omega_be(X)	# nu_*e (not used as of now)
 
 
 # Consolidated constant to reduce clutter
-C = lambda T: nu_ai(T) * aspect**(3.0/2) * nu_ei(T) / nu_ii(T)
+C = lambda X: nu_ai(X) * aspect**(3.0/2) * nu_ei(X) / nu_ii(X)
 
-print C(1e3)
+#print C(0.5)
 
