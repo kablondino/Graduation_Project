da6acac325daa1e5137b27536d2e66e53b2e9df2
kablondino <kablondino@gmail.com>
2017-10-17 17:54:00 +0200

    Figured out how to add

--- Model_Source/parameters.py
+++ Model_Source/parameters.py
@@ -13,11 +13,11 @@ mu_0 = 4*pi*1.0e-7
 R_cx = 1.0e-14
 
 # Plasma variables n_e, T_i, n_0
-n = lambda x: (1 - x) * 1.0e19
-T = lambda x: (1 - x) * 2.0e3
+def n(x): return (1 - x) * 1.0e19
+def T(x): return (1 - x) * 2.0e3
 np = 1.0e19
 Tp = 1.0e3
-n_0 = lambda x: 4.0e17 * a_in0 * ( T(x) / 100 )**(3.0/4)
+def n_0(x): return 4.0e17 * a_in0 * ( T(x) / 100 )**(3.0/4)
 
 # ASDEX-U specifications
 a_v = 0.8	# Vertical minor radius
@@ -39,36 +39,36 @@ L_nn = L_n / W_ped
 L_Tn = L_T / W_ped
 
 # Parameters
-e_p = lambda X: 1.0 + (m_i*n(X) + m_e*n(X)) / (epsilon_0 * B**2)
+def e_p(X): return 1.0 + (m_i*n(X) + m_e*n(X)) / (epsilon_0 * B**2)
 
-v_Ti = lambda X: (2 * e * T(X) / m_i)**(1.0/2)
+def v_Ti(X): return (2 * e * T(X) / m_i)**(1.0/2)
 
-v_Te = lambda X: (2 * e * T(X) / m_e)**(1.0/2)
+def v_Te(X): return (2 * e * T(X) / m_e)**(1.0/2)
 
-rho_pi = lambda X: m_i * v_Ti(X) / (e * B_p)
+def rho_pi(X): return m_i * v_Ti(X) / (e * B_p)
 
-rho_pe = lambda X: m_e * v_Te(X) / (e * B_p)
+def rho_pe(X): return m_e * v_Te(X) / (e * B_p)
 
-omega_bi = lambda X: aspect**(3.0/2) * v_Ti(X) / (q * R)
+def omega_bi(X): return aspect**(3.0/2) * v_Ti(X) / (q * R)
 
-omega_be = lambda X: aspect**(3.0/2) * v_Te(X) / (q * R)
+def omega_be(X): return aspect**(3.0/2) * v_Te(X) / (q * R)
 
 # Frequencies
-nu_ei = lambda X: 1.33e5 * (n(X) * 1.0e-20) / (T(X) * 1.0e-3)**(3.0/2)
+def nu_ei(X): return 1.33e5 * (n(X) * 1.0e-20) / (T(X) * 1.0e-3)**(3.0/2)
 
-nu_ii = lambda X: 1.2 * (m_e / m_i)**(1.0/2) * nu_ei(X)
+def nu_ii(X): return 1.2 * (m_e / m_i)**(1.0/2) * nu_ei(X)
 
-nu_in0 = lambda X: a_in0 * omega_bi(X)
+def nu_in0(X): return a_in0 * omega_bi(X)
 
-nu_eff = lambda X: nu_ii(X) + nu_in0(X)
+def nu_eff(X): return nu_ii(X) + nu_in0(X)
 
-nu_ai = lambda X: nu_ii(X) / omega_bi(X)	# nu_*i
+def nu_ai(X): return nu_ii(X) / omega_bi(X)	# nu_*i
 
-nu_ae = lambda X: nu_ei(X) / omega_be(X)	# nu_*e (not used as of now)
+def nu_ae(X): return nu_ei(X) / omega_be(X)	# nu_*e (not used as of now)
 
 
 # Consolidated constant to reduce clutter
-C = lambda X: nu_ai(X) * aspect**(3.0/2) * nu_ei(X) / nu_ii(X)
+def C(X): return nu_ai(X) * aspect**(3.0/2) * nu_ei(X) / nu_ii(X)
 
 #print C(0.5)
 
