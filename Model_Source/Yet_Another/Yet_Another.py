be9bbaa376773ca2934bcaa442675d695edaff9c
kablondino <kablondino@gmail.com>
2017-11-03 18:30:14 +0100

    Splitting all Z-dependent and non-Z terms

--- Model_Source/Yet_Another/Yet_Another.py
+++ Model_Source/Yet_Another/Yet_Another.py
@@ -13,6 +13,7 @@ V = FunctionSpace(mesh, 'P', 1)
 
 # Set up independent and dependent variables
 x = SpatialCoordinate(mesh)
+z = TrialFunction(V)
 v = TestFunction(V)
 Z = Function(V)
 
@@ -26,19 +27,29 @@ bc = DirichletBC(V, Z_D, boundary)
 # Write the coefficients over from the coefficients file
 g_n_an, g_T_an, g_Z_an = coef.g_n_an, coef.g_T_an, coef.g_Z_an
 g_n_cx, g_T_cx, g_Z_cx = coef.g_n_cx, coef.g_T_cx, coef.g_Z_cx
+g_n_bulk, g_T_bulk, g_Z_bulk = coef.g_n_bulk, coef.g_T_bulk, coef.g_Z_bulk
 f_OL = coef.f_OL
 
 #f = Expression('pow(B_p,2)*((g_n_an - g_n_cx)*L_n + (g_T_an - g_T_cx)*L_T)', degree=2, B_p=B_p, g_n_an=g_n_an, g_n_cx=g_n_cx, L_n=L_n, g_T_an=g_T_an, g_T_cx=g_T_cx, L_T=L_T)
-f = Expression('f_OL', degree=2, f_OL=f_OL)
+f = Expression('(g_n_an - g_n_cx)*L_n + (g_T_an - g_T_cx)*L_T', degree=1, g_n_an=g_n_an, g_n_cx=g_n_cx, L_n=L_n, g_T_an=g_T_an, g_T_cx=g_T_cx, L_T=L_T)
+g = Expression('(-g_Z_an + g_Z_cx + g_Z_bulk)*Z + g_n_bulk*L_n + g_T_bulk*L_T - f_OL', degree=1, g_n_bulk=g_n_bulk, g_T_bulk=g_T_bulk, g_Z_bulk=g_Z_bulk, L_n=L_n, L_T=L_T, g_Z_an=g_Z_an, g_Z_cx=g_Z_cx, f_OL=f_OL, Z=Z)
 print f
 
-tol = 1.0e-4
+#Z = interpolate(Expression('1 - tanh(L/2.0*(x[0] - 1))', degree=1, L=L), V)
+F = Z*v*dx + (-m_i / (charge*B_p**2))*rho_pi**(-1) * dot(grad(Z), grad(v))*dx + f*v*dx
 
-F = (m_i/charge)*n*Temp*rho_pi * dot(grad(Z), grad(v))*dx + f*v*dx
-J = derivative(F, x, dx)
+J = derivative(F, Z, v)
+print J
 
-solver = NewtonSolver
+#problem = NonlinearVariationalProblem(F, Z, bc, J)
 
-solve(F == 0, Z, bc)
+#solver = NonlinearVariationalSolver(problem)
+
+#solver.solve()
+
+#vtkfile = File('solution.pvd')
+
 solve(F == 0, Z, bc, solver_parameters={"newton_solver":{"maximum_iterations":1000}})
+
+#vtkfile << Z
 
