5db08ba46c7b9e7b29e45aae738fb49c02d8a24f
kablondino <kablondino@gmail.com>
2017-11-29 15:47:04 +0100

    Working, mostly. Right boundary for temp isnt

--- FiPy_Model/density_temp_couple.py
+++ FiPy_Model/density_temp_couple.py
@@ -1,81 +1,69 @@
-## This file calculates the density AND temperature with naive diffusivity
-
-from fipy import Variable, CellVariable, Grid1D, TransientTerm, DiffusionTerm, ExplicitDiffusionTerm, ConvectionTerm, ImplicitSourceTerm, Viewer
+from fipy import Grid1D, CellVariable, TransientTerm, DiffusionTerm, Viewer, ConvectionTerm, ImplicitSourceTerm
 
 from fipy.tools import numerix
 
-L = 5.0
 nx = 100
-dx = L / nx
-mesh = Grid1D(nx=nx, dx=dx)
-
-# Parameters from core, SOL, etc.
-gamma = 5.0/3.0
-Gamma_c = -4.0/5.0
-q_c = -4.0
-zeta = 0.5
-lambda_n = 5.0/4.0
-lambda_T = 3.0/2.0
-
-# Diffusivity function(s) OR constant
-diffusivity = 5
-
-timeStepDuration = 9 * dx**2 / (2*diffusivity)
-steps = 50
+L = 5.0
+mesh = Grid1D(nx=nx, Lx=L)
 
 x = mesh.cellCenters[0]
 
-# Solution variable
-density = CellVariable(name=r"$n$", mesh=mesh)
-temperature = CellVariable(name=r"$T$", mesh=mesh)
+# Parameters
+D = 1.0
+zeta = 0.5
+Gamma_c = -4.0/5.0
+q_c = -4.0
+gamma = 5.0/3.0
+lambda_n = 5.0/4.0
+lambda_T = 3.0/2.0
 
 # Initial Conditions
-density_initial = CellVariable(name="Initial Density", mesh=mesh, value=-Gamma_c*lambda_n / diffusivity * (1 + x/lambda_n))
-density.setValue(density_initial)
+density0 = CellVariable(name=r"$n_0$", mesh=mesh, value=-(Gamma_c*lambda_n / D) * (1 + x/0.5))
 
-temp_initial = CellVariable(name="Initial Temperature", mesh=mesh, value= q_c * (gamma - 1) / Gamma_c * (1 - (lambda_n / (zeta*lambda_T + lambda_n)) * (1 + x/lambda_n)**(-zeta)))
-temperature.setValue(temp_initial)
+temp0 = CellVariable(name=r"$T_0", mesh=mesh, value = q_c*((gamma - 1) / Gamma_c) * (1 - lambda_n / (zeta*lambda_T + lambda_n)*(1 + x/lambda_n)**(-zeta)))
 
+# Variable Declarations
+density = CellVariable(name=r"$n$", mesh=mesh, hasOld=True, value=density0)
+
+temperature = CellVariable(name=r"$T$", mesh=mesh, hasOld=True, value=temp0)
 
 ## Density Boundary Conditions:
-#	d/dx(n(0,t)) == n(0,t) / lambda_n
-#	d/dx(n(L,t)) == -Gamma_c(t) / D
-density_left_neumann = density / lambda_n
-density.faceGrad.constrain(density_left_neumann, mesh.facesLeft)
-density_right_neumann = -Gamma_c / diffusivity
-density.constrain(density_right_neumann, mesh.facesRight)
+#	D*n(0,t) / lambda_n + D*d/dx(n(0,t)) == 0
+#	Gamma_c(t) + D*d/dx(n(L,t)) == 0
+density_left = density / lambda_n
+density.faceGrad.constrain(density_left, mesh.facesLeft)
+density_right = -Gamma_c / D
+density.faceGrad.constrain(density_right, mesh.facesRight)
 
 ## Temperature Boundary Conditions:
 #	d/dx(T(0,t)) = T / (lambda_T*(1 + (zeta+1)*(1/n)*dn/dx))
 #	d/dx(T(L,t)) = (T*Gamma_c - (gamma - 1)*q_c) / (diffusivity * n)
-temp_left_neumann = temperature / (lambda_T*(1 + (zeta+1)*(1/density)*density.grad))		# FIX!
-temperature.faceGrad.constrain(temp_left_neumann, mesh.facesLeft)
-#print temp_left_neumann		# How to represent derivative, not grad!
+temp_left = temperature / lambda_T
+temperature.faceGrad.constrain(temp_left, mesh.facesLeft)
+# THE FOLLOWING NEEDS TO BE FIXED, since the index gets messed up
+temp_right = (zeta / D)#*(temperature*Gamma_c - (gamma - 1)*q_c) / density
+temperature.faceGrad.constrain(temp_right, mesh.facesRight)
 
-temp_right_neumann = (zeta*(temperature*Gamma_c - (gamma-1)*q_c)) / (diffusivity*density)	# FIX!
-temperature.faceGrad.constrain(temp_right_neumann, mesh.facesLeft)
+# Density Equation
+density_equation = TransientTerm(var=density) == DiffusionTerm(coeff=5.0, var=density)
+
+# Temperature Equation
+S_T = ((zeta+1)/zeta)*(D/density)*numerix.dot(density.grad,temperature.grad)
+temp_equation = TransientTerm(var=temperature) == DiffusionTerm(coeff=5.0/0.5, var=temperature) + S_T
 
 
-# Set the equations
-density_equation = TransientTerm(var=density) == DiffusionTerm(coeff=diffusivity, var=density)
+# Coupled Equation
+full_equation = density_equation & temp_equation
+#print full_equation
 
-S_T = (zeta + 1) / zeta * diffusivity / density * (density.grad.mag * temperature.grad.mag)
-#print S_T
-
-#print S_T * ConvectionTerm((zeta,0))# * ConvectionTerm((1.0,), var=temperature)
-temp_equation = TransientTerm(var=temperature) == ExplicitDiffusionTerm(coeff=diffusivity / zeta, var=temperature) + S_T
-
-semifull_eq = density_equation & temp_equation
-print semifull_eq
- 
-#if __name__ == '__main__':
-#	viewer = Viewer((density, temperature), datamin=0.0, datamax=max(temp_initial))
-#
-#for step in range(steps):
-#	semifull_eq.solve(dt=timeStepDuration)
-#	if __name__ == '__main__':
-#		viewer.plot()
+viewer = Viewer((density, temperature), datamin=0.0)
 
 if __name__ == '__main__':
-	raw_input("Pause. Press <return> to continue.")
+	for t in range(100):
+		density.updateOld()
+		temperature.updateOld()
+		full_equation.solve(dt=1.0e-3)
+		viewer.plot()
+
+	raw_input("End of Program. <return> to continue...")
 
