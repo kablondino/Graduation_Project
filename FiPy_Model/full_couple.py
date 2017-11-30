c4c60482b77d812b624f4ad3b4428bd2ce6f720d
kablondino <kablondino@gmail.com>
2017-11-30 15:32:03 +0100

    Semi-working model with strange behavior

--- FiPy_Model/full_couple.py
+++ FiPy_Model/full_couple.py
@@ -18,6 +18,9 @@ lambda_n = 5.0/4.0
 lambda_T = 3.0/2.0
 lambda_Z = 5.0/4.0
 
+D_max = 2.0
+D_min = 2.0/5.0
+
 epsilon = 1.0/25.0
 mu = 1.0/20.0
 
@@ -38,87 +41,95 @@ temperature = CellVariable(name=r"$T$", mesh=mesh, hasOld=True)
 Z = CellVariable(name=r"$Z$", mesh=mesh, hasOld=True)
 
 ## Diffusivity
-D_max = 2.0
-D_min = 2.0/5.0
-#D = CellVariable(name=r"$D$", mesh=mesh, hasOld=True)
-D = 5.0
-#def D(aZ):
-#	return (D_max + D_min) / 2.0 + ((D_max - D_min)*numerix.tanh(aZ)) / 2.0
+D = CellVariable(name=r"$D$", mesh=mesh, hasOld=True)
 
 
-# ----------------- Initial Conditions --------------------
-density0 = CellVariable(name=r"$n_0$", mesh=mesh, value=-(Gamma_c*lambda_n / D) * (1 + x/0.5))
+# ------------- Initial Conditions and Diffusivity---------
+Z0 = Z_S*(1 - numerix.tanh((L*x - L) / 2))
+Z.setValue(Z0)
+
+# Zohm's model
+#D.setValue((D_max + D_min) / 2.0 + ((D_max - D_min)*numerix.tanh(Z)) / 2.0)
+# Stap's Model
+alpha_sup = 1.0
+#D = (D_min + (D_max - D_min) / (1 + alpha_sup*(Z.grad.mag)**2))
+# Flow-Shear Model
+a1, a2, a3 = 1.0, 1.0, 1.0
+D.setValue(D_min + (D_max - D_min) / (1 + a1*Z**2 + a2*Z*(Z.grad) + a3*(Z.grad)**2))
+
+density0 = CellVariable(name=r"$n_0$", mesh=mesh, value=-(Gamma_c*lambda_n / D) * (1 + x/lambda_n))
 density.setValue(density0)
 
 temp0 = CellVariable(name=r"$T_0", mesh=mesh, value = q_c*((gamma - 1) / Gamma_c) * (1 - lambda_n / (zeta*lambda_T + lambda_n)*(1 + x/lambda_n)**(-zeta)))
 temperature.setValue(temp0)
 
-Z0 = Z_S*(1 - numerix.tanh((L*x - L) / 2))
-Z.setValue(Z0)
-
-#D0 = (D_max + D_min) / 2.0 + ((D_max - D_min)*numerix.tanh(Z)) / 2.0
-#D.setValue(D0)
-print D
 
 # ----------------- Boundary Conditions -------------------
 # Density Boundary Conditions:
 #	d/dx(n(0,t)) == n / lambda_n
 #	d/dx(n(L,t)) == D / Gamma_c
-density_left = density / lambda_n
+density_left = density[0] / lambda_n
 density.faceGrad.constrain(density_left, mesh.facesLeft)
 
-density_right = -Gamma_c / D
+density_right = -Gamma_c / D[nx-1]
 density.faceGrad.constrain(density_right, mesh.facesRight)
 
 # Temperature Boundary Conditions:
 #	d/dx(T(0,t)) = T / lambda_T
 #	d/dx(T(L,t)) = (T*Gamma_c - (gamma - 1)*q_c) / (diffusivity * n)
-temp_left = temperature / lambda_T
+temp_left = temperature[0] / lambda_T
 temperature.faceGrad.constrain(temp_left, mesh.facesLeft)
 # THE FOLLOWING NEEDS TO BE FIXED, since the index gets messed up
-temp_right = (zeta / D)#*(temperature*Gamma_c - (gamma - 1)*q_c) / density
+temp_right = (zeta / D[nx-1])*(temperature[nx-1]*Gamma_c - (gamma - 1)*q_c) / density[nx-1]
 temperature.faceGrad.constrain(temp_right, mesh.facesRight)
 
 # Z Boundary Conditions:
-#	d/dx(Z(0,t)) = Z / lambda_Z
+#	d/dx(Z(0,t)) == Z / lambda_Z
+#	d^2/dx^2(Z(0,t)) == 0
 #	mu*D/epsilon * d/dx(Z(L,t)) == 0  ---->  Z == 0
-Z_left = Z / lambda_Z
+Z_left = Z[0] / lambda_Z
+Z.constrain(0.0, mesh.facesRight)
 Z.faceGrad.constrain(Z_left, mesh.facesLeft)
 
-Z.constrain(0.0, mesh.facesRight)
+Z.grad.faceGrad.constrain(0.0, mesh.facesLeft)
 
 
 # ----------------- PDE Declarations ----------------------
 # Density Equation
-density_equation = TransientTerm(var=density) == DiffusionTerm(coeff=5.0, var=density)
+density_equation = TransientTerm(var=density) == DiffusionTerm(coeff=D, var=density)
+print D
 
 # Temperature Equation
 S_T = ((zeta+1)/zeta)*(D/density)*numerix.dot(density.grad,temperature.grad)
+print S_T
 temp_equation = TransientTerm(var=temperature) == DiffusionTerm(coeff=5.0/0.5, var=temperature) + S_T
 
 # Z Equation
 G = a + b*(Z - Z_S) + c*(Z - Z_S)**3
 #S_Z = numerix.dot((c_n*temperature / density**2),density.grad) + numerix.dot((c_T / density), temperature.grad) + G
 #S_Z = ConvectionTerm(coeff=((c_n*temperature/density**2),), var=density) + ConvectionTerm(coeff=((c_T / density),), var=temperature) + G
-S_Z = G# + -density * temperature.grad
-Z_equation = TransientTerm(coeff=epsilon, var=Z) == DiffusionTerm(coeff=mu*D, var=Z) + S_Z
+S_Z = G + (c_T*temperature / density**2)*density.grad.mag + (c_T / density)*temperature.grad.mag
+Z_equation = TransientTerm(coeff=epsilon, var=Z) == DiffusionTerm(coeff=mu*D/epsilon, var=Z) + S_Z
+
+## ALTERNATE Z Equation
+#S_Za = B_theta**2*((g_n_an - g_n_cx - g_n_bulk)*density.grad.mag/density + (g_T_an - g_T_cx - g_T_bulk)*temperature.grad.mag/temperature + (g_Z_an - g_Z_cx - g_Z_bulk)*Z - f_OL)
+#Z_equation = TransientTerm(coeff=, var=Z) == DiffusionTerm(coeff=, var=Z) + S_Za
 
 
 # Fully-Coupled Equation
 full_equation = density_equation & temp_equation & Z_equation
-print full_equation
+
+
+#initial_viewer = Viewer((D,Z))
+#raw_input("Pause for Initial")
 
 if __name__ == '__main__':
 	viewer = Viewer((density, temperature, Z))
-	initial_view = Viewer((Z))
-	raw_input("Pause")
+	for t in range(100):
+		density.updateOld()
+		temperature.updateOld()
+		full_equation.solve(dt=1.0e-2)
+		viewer.plot()
 
-#if __name__ == '__main__':
-#	for t in range(100):
-#		density.updateOld()
-#		temperature.updateOld()
-#		full_equation.solve(dt=1.0e-3)
-#		viewer.plot()
-#
-#	raw_input("End of Program. <return> to continue...")
+	raw_input("End of Program. <return> to continue...")
 
