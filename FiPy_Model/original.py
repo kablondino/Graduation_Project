6d8c5ed99911b3e1bb6499b073a7d22ca28afce8
kablondino <kablondino@gmail.com>
2018-01-10 02:34:37 +0100

    Slight rewrite of equations after sending email

--- FiPy_Model/original.py
+++ FiPy_Model/original.py
@@ -72,19 +72,17 @@ Z.faceGrad.divergence.constrain(0.0, mesh.facesLeft)
 
 # ----------------- PDE Declarations ----------------------
 # Density Equation
-density_equation = TransientTerm(var=density) == DiffusionTerm(coeff=Diffusivity, var=density)
+density_equation = TransientTerm(coeff=1.0, var=density) == DiffusionTerm(coeff=Diffusivity, var=density)
 
 # Temperature Equation
-chi = Diffusivity / (zeta*(gamma - 1.0))
-#temp_equation = TransientTerm(coeff=(density/(gamma - 1.0)), var=temperature) == DiffusionTerm(coeff=chi*density, var=temperature) + DiffusionTerm(coeff=Diffusivity*temperature, var=density)
-S_T = ((zeta+1.0)/zeta)*(Diffusivity/density) * numerix.dot(density.grad, temperature.grad) + (Diffusivity/density)*numerix.dot(temperature, density.faceGrad.divergence)
-temp_equation = TransientTerm(var=temperature) == DiffusionTerm(coeff=Diffusivity/zeta, var=temperature) + S_T
+temp_equation = TransientTerm(coeff=density, var=temperature) == DiffusionTerm(coeff=(Diffusivity*density/zeta), var=temperature) + DiffusionTerm(coeff=Diffusivity*temperature, var=density)
+#S_T = ((zeta + 1.0)/zeta) * (Diffusivity / density) * numerix.dot(density.grad, temperature.grad) + (Diffusivity*temperature)/density * density.faceGrad.divergence
+#temp_equation = TransientTerm(coeff=1.0, var=temperature) == DiffusionTerm(coeff=(Diffusivity/zeta), var=temperature) + S_T
 
 # Z Equation
 G = a + b*(Z - Z_S) + c*(Z - Z_S)**3
-S_Z = (c_T / density) * temperature.grad[0] + (c_n*temperature / density**2) * density.grad[0] + G
-Z_equation = TransientTerm(coeff=epsilon, var=Z) == DiffusionTerm(coeff=mu*Diffusivity/epsilon, var=Z) + S_Z
-
+S_Z = ((c_n*temperature) / density**2) * density.grad[0] + (c_T / density) * temperature.grad[0] + G
+Z_equation = TransientTerm(coeff=epsilon, var=Z) == DiffusionTerm(coeff=mu, var=Z) + S_Z
 
 # Fully-Coupled Equation
 full_equation = density_equation & temp_equation & Z_equation
@@ -97,17 +95,17 @@ x_min, x_max, y_min, y_max = 0.0, L, -1.0, 5.0
 #initial_viewer = Viewer((density, temperature, Z, Diffusivity), xmin=x_min, xmax=x_max, datamin=y_min, datamax=y_max, legend='best')
 #raw_input("Pause for Initial")
 
-timeStep = 0.9*epsilon / 5
+timeStep = epsilon / 3.0
 print timeStep
 
-#if __name__ == '__main__':
-#	viewer = Viewer((density, temperature, -Z, Diffusivity), datamin=y_min, datamax=y_max, xmin=x_min, xmax=x_max, legend='best')
-#	for t in range(100):
-#		density.updateOld(); temperature.updateOld(); Z.updateOld()
-#		#Diffusivity.updateOld()	# Remanent of when it was a CellVariable
-##		TSVViewer(vars=(density, temperature, Z, Diffusivity)).plot(filename="full_solution/full"+str(t)+".tsv")
-#		full_equation.solve(dt=timeStep)
-#		viewer.plot()
-#
-#	raw_input("End of Program. <return> to continue...")
+if __name__ == '__main__':
+	viewer = Viewer((density, temperature, Z), xmin=x_min, xmax=x_max, legend='best')
+	for t in range(100):
+		density.updateOld(); temperature.updateOld(); Z.updateOld()
+		#Diffusivity.updateOld()	# Remanent of when it was a CellVariable
+#		TSVViewer(vars=(density, temperature, Z, Diffusivity)).plot(filename="full_solution/full"+str(t)+".tsv")
+		full_equation.solve(dt=timeStep)
+		viewer.plot()
+
+	raw_input("End of Program. <return> to continue...")
 
