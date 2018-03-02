3563f32430315ce0623b5a0e74f5c0d909c333b4
kablondino <kablondino@gmail.com>
2018-03-02 17:01:18 +0100

    Added more comments and organization

--- FiPy_Model/coeffs.py
+++ FiPy_Model/coeffs.py
@@ -12,14 +12,20 @@ e_p = 1.0 + (m_i*density + m_e*density) / (epsilon_0 * B**2)
 
 # Neutrals density in use for CX friction
 n_0 = CellVariable(name=r"$n_0$", mesh=mesh)
+
+# Thermal velocities (most probable)
 v_Ti = CellVariable(name=r"$v_{th,i}$", mesh=mesh)
 v_Te = CellVariable(name=r"$v_{th,e}$", mesh=mesh)
+
+# Poloidal gyro-(Larmor) radii
 rho_pi = CellVariable(name=r"$\rho_{\theta i}$", mesh=mesh)
 rho_pe = CellVariable(name=r"$\rho_{\theta e}$", mesh=mesh)
+
+# Banana orbit bounce frequencies
 omega_bi = CellVariable(name=r"$\omega_{bi}$", mesh=mesh)
 omega_be = CellVariable(name=r"$\omega_{be}$", mesh=mesh)
 
-## Collision Frequencies
+# Collision Frequencies
 nu_ei = CellVariable(name=r"$\nu_{ei}$", mesh=mesh)
 nu_ii = CellVariable(name=r"$\nu_{ii}$", mesh=mesh)
 nu_in0 = CellVariable(name=r"$\nu_{i0}$", mesh=mesh)
@@ -60,29 +66,30 @@ def update_g_coeffs():
 	# Neutrals density in use for CX friction
 	n_0.setValue(4.0e17 * a_in0 * (temperature / 100.0)**(3.0/4.0))
 
+	# Thermal velocities (most probable)
 	v_Ti.setValue((2.0 * charge * temperature / m_i)**(1.0/2.0))
-
 	v_Te.setValue((2.0 * charge * temperature / m_e)**(1.0/2.0))
 
+	# Poloidal gyro-(Larmor) radii
 	rho_pi.setValue(m_i * v_Ti / (charge * B_theta))
-
 	rho_pe.setValue(m_e * v_Te / (charge * B_theta))
 
+	# Banana orbit bounce frequencies
 	omega_bi.setValue(aspect**(3.0/2.0) * v_Ti / (q * R))
-
 	omega_be.setValue(aspect**(3.0/2.0) * v_Te / (q * R))
 
-	## Collision Frequencies
-	nu_ei.setValue(((density) / (temperature))**(3.0/2.0))
-
+	# Collision frequencies within electrons and ions
+	nu_ei.setValue((density / temperature)**(3.0/2.0))
 	nu_ii.setValue(1.2 * (m_e / m_i)**(1.0/2.0) * nu_ei)
 
+	# Collision frequency of trapped ions and neutrals
 	nu_in0.setValue(a_in0 * omega_bi)
 
+	# Effective detrapping frequency
 	nu_eff.setValue(nu_ii + nu_in0)
 
+	# Effective collision frequencies
 	nu_ai.setValue(nu_ii / omega_bi)	# nu_*i
-
 	nu_ae.setValue(nu_ei / omega_be)	# nu_*e (not used as of now)
 
 	## Consolidated constant to reduce clutter, listed as N on reference
