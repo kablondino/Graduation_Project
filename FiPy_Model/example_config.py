24b41052f6593226b51ddebcdab29dcfcd878765
kablondino <kablondino@gmail.com>
2018-03-21 16:34:13 +0100

    Small cleanup; added ploty_max

--- FiPy_Model/example_config.py
+++ FiPy_Model/example_config.py
@@ -6,7 +6,6 @@
 # Particle and heat flux from the core
 Gamma_c = -4.0 / 5.0
 q_c = -4.0
-print type(Gamma_c)
 
 # Number of cells
 nx = 200
@@ -23,7 +22,7 @@ timeStep_denom = 12.0
 
 # Choose the Diffusivity model, as a string (case does not matter)
 # D_Zohm, D_Staps, and D_Shear are the possibilities
-D_choice = "D_Staps"
+D_choice = "D_Shear"
 
 # Boolean, to choose what mode to have as initial conditions
 initial_H_mode = False
@@ -39,13 +38,14 @@ numerical_choice = "Staps"
 res_tol = 1.0e-6
 
 # Plot details
-plot_title = "L--Mode Start; Staps' parameters; \n" +\
+plot_title = "L--Mode Start; Staps' parameters; Flow-Shear $D$\n" +\
 		r"$\Gamma_c =$" +str(Gamma_c)+ \
 		r", $T = $"+str(total_timeSteps)+ r", $\Delta t = \mu / $"\
 		+str(timeStep_denom)
 
 # Maximum x on the plots
 plotx_max = L
+ploty_max = None
 # Maximum and minimum y values on the plots
 aux1y_min = None
 aux1y_max = None
@@ -53,8 +53,8 @@ aux2y_min = None
 aux2y_max = None
 
 # Should the results be saved to file?
-save_plots = True
-save_TSVs = True
+save_plots = False
+save_TSVs = False
 
-save_directory = "Standard_4_5"
+save_directory = ""
 
