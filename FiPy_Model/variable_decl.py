735f3f1bb135513ac7bb637b97116b9ffcb93bdd
kablondino <kablondino@gmail.com>
2018-03-22 21:15:01 +0100

    BIG rewrite; NEW initial conditions

--- FiPy_Model/variable_decl.py
+++ FiPy_Model/variable_decl.py
@@ -22,6 +22,7 @@ mesh = Grid1D(nx=config.nx, Lx=config.L)
 x = mesh.cellCenters[0] # Cell position
 X = mesh.faceCenters[0] # Face position, if needed
 
+
 # ----------------- Variable Declarations -----------------
 density = CellVariable(name=r"$n$", mesh=mesh, hasOld=True)
 
@@ -33,16 +34,33 @@ Z = CellVariable(name=r"$Z$", mesh=mesh, hasOld=True)
 
 Diffusivity = CellVariable(name=r"$D$", mesh=mesh, hasOld=True)
 
-# ----------- Initial Conditions of Z ---------------------
-if config.initial_H_mode == False:
-	Z.setValue(0.0) # L--mode
-elif config.initial_H_mode == True:
-	Z.setValue(Z_S*(1.0 - numerix.tanh(\
-			(config.L*x - config.L) / 2.0))) # H--mode
-else:
-	print "The check for initial_H_mode is not working.... stop."
 
-# ----------------- Diffusivities -------------------------
+# ---------------- Set Initial Conditions -----------------
+# Initial conditions for L--mode
+if config.initial_H_mode == False:
+	density.setValue(1.5*x / 4.0 + 0.5)
+
+	temperature.setValue(0.0125*x**2 + 0.2*x + 1.2)
+
+	Z.setValue(0.0)
+
+# Initial conditions for H--mode
+elif config.initial_H_mode == True:
+	density.setValue((0.5/1.5)*x + 0.5)
+	density.setValue(3.0*x - 3.5, where = x > 1.5)
+	density.setValue((0.5/1.5)*x + (11.0/6.0)*1.0, where = x > 2.0)
+
+	temperature.setValue(0.2*x + 1.2)
+	temperature.setValue(1.8*x - 1.2, where = x > 1.5)
+	temperature.setValue(0.2*x + 2.0, where = x > 2.0)
+
+	Z.setValue(-3.0 / (1.0 + numerix.exp(12.0*(x - 1.75))))
+
+else:
+	print "Something went horribly wrong in choosing initial conditions."
+
+
+# ----------------- Set Diffusivity Model -----------------
 # Itohs'/Zohm's model
 D_Zohm = (D_max + D_min) / 2.0 + ((D_max - D_min)*numerix.tanh(Z)) / 2.0
 # Stap's Model
@@ -69,43 +87,18 @@ else:
 Diffusivity.setValue(D_choice_local)
 print "The diffusivity model is set to " + str(config.D_choice)
 
-# --------- Set Initial Conditions for n and T ------------
-if config.SI_units == True:					# NEEDS TO BE FIXED!
-	density_si_coeff = 0.05		# x 10^20 m^-3
-	temp_si_coeff = 0.5			# eV
-elif config.SI_units == False:
-	density_si_coeff = 1.0		# m^-3
-	temp_si_coeff = 1.0			# eV
-else:
-	print "Something went horribly wrong when choosing the units."
 
-# Initial conditions for L--mode
-if config.initial_H_mode == False:
-#	density.setValue(-(density_si_coeff*config.Gamma_c*lambda_n / Diffusivity)\
+U.setValue(density*temperature / (gamma - 1.0))
+
+
+# ------- Old definitions, which requires Diffusivity to ALREADY be set ------
+#	density.setValue(-(config.Gamma_c*lambda_n / Diffusivity)\
 #			* (1.0 + x/lambda_n))
-	density.setValue((2.0-0.5)*x / config.L + 0.5)
-
-#	temperature.setValue(temp_si_coeff*config.q_c\
+#	temperature.setValue(config.q_c\
 #			*((gamma - 1.0) / config.Gamma_c)\
 #			*(1.0 - lambda_n / (zeta*lambda_T + lambda_n)\
 #			*(1.0 + x/lambda_n)**(-zeta)))
-	temperature.setValue(3.0 - 1.0*density**-zeta)
 
-# Initial conditions for H--mode
-elif config.initial_H_mode == True:
-#	density.setValue(-(density_si_coeff*config.Gamma_c*lambda_n\
-#			/ Diffusivity) * (1.0 + x/lambda_n))
-	density.setValue((0.5/1.5)*x + 0.5)
-	density.setValue(3.0*x - 3.5, where = x > 1.5)
-	density.setValue((0.5/1.5)*x + (11.0/6.0)*1.0, where = x > 2.0)
-
-#	temperature.setValue(temp_si_coeff*config.q_c*((gamma - 1.0)\
-#			/ config.Gamma_c) * (1.0 - lambda_n / (zeta*lambda_T + lambda_n)\
-#			*(1.0 + x/lambda_n)**(-zeta)))
-	temperature.setValue(3.0 - 1.0*density**-zeta)
-
-else:
-	print "Something went horribly wrong in choosing initial conditions."
-
-U.setValue(density*temperature / (gamma - 1.0))
+#	Z.setValue(Z_S*(1.0 - numerix.tanh(\
+#			(config.L*x - config.L) / 2.0)))	# OLD, by Staps
 
