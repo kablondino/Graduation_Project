3692bcb6fbaa584caa7dda53581e72933b291b98
kablondino <kablondino@gmail.com>
2018-04-06 19:46:00 +0200

    Better signs; still needs investigating

--- FiPy_Model/calculate_coeffs.py
+++ FiPy_Model/calculate_coeffs.py
@@ -60,7 +60,7 @@ def calculate_coeffs():
 	## Electron Anomalous Diffusion
 	D_an.setValue(aspect**2 * numerix.sqrt(pi) * rho_pe * temperature\
 			/ (2*a_m * B))
-	g_n_an.setValue(charge*density*D_an)						# [A m^-2]
+	g_n_an.setValue(-charge*density*D_an)						# [A m^-2]
 	g_T_an.setValue(g_n_an * alpha_an)							# [A m^-2]
 	g_Z_an.setValue(g_n_an / rho_pi)							# [A m^-1]
 
@@ -85,19 +85,20 @@ def calculate_coeffs():
 	plasma_disp.setValue(numpy.imag(1j * numerix.sqrt(pi)\
 			* scipy.special.wofz(Z + 1j*nu_ii / omega_t)))
 	D_bulk.setValue(aspect**2 * rho_pi * temperature\
-			/ ((x + a_m) * B * numerix.sqrt(pi)))				# [m^2 s^-1]
+			/ ((a_m - x) * B * numerix.sqrt(pi)))				# [m^2 s^-1]
 
-	Gamma_bulk.setValue(density * D_bulk * (1.0/L + Z/rho_pi)\
+	Gamma_bulk.setValue(density * D_bulk * (1.0e-3/L + Z/rho_pi)\
 			* plasma_disp)										# [m^-2 s^-1]
 
 
 	## Ion Orbit Loss
 	g_OL.setValue(-charge * density * nu_ii * nu_ai * rho_pi)	# [A m^-2]
+#	radical_OL = 
 
-	radical_OL = numerix.sqrt(nu_ai + Z**4 + ((x)/w_bi)**4)
-
-	Gamma_OL.setValue(g_OL * numerix.exp(-radical_OL)\
-			/ (charge*radical_OL))								# [m^-2 s^-1]
+	Gamma_OL.setValue(g_OL * numerix.exp(-numerix.sqrt(\
+			nu_ai + (Z)**4 + ((x)/w_bi)**4))\
+			/ (charge*numerix.sqrt(nu_ai + (Z)**4\
+			+ ((x)/w_bi)**4)))								# [m^-2 s^-1]
 
 	Z_transient_coeff.setValue(m_i * density * temperature\
 			/ (charge* rho_pi * B**2))
